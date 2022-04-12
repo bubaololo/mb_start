18448af4c90a2b3ca39ce80f4d23bca3209cb43c
bubaololo <79483014+bubaololo@users.noreply.github.com>
2022-04-12 10:24:05 +0200

    adaptive ready

--- js/main.js
+++ js/main.js
@@ -2,17 +2,30 @@
 
 document.querySelector(".menu__icon").addEventListener("click", (event) => {
     document
-      .querySelectorAll(".line, .header__menu")
+      .querySelectorAll(".burger_line, .header__menu")
       .forEach((element) => element.classList.toggle("active"));
     document.querySelector("html").classList.toggle("lock");
   });
 
-  // let cards = document.querySelectorAll('.flip-card-inner');
 
-  // cards.forEach((card) => card.addEventListener('click', flip));
-  // function flip(){
-  //   this.classList.toggle('is-flipped');
-  // }
 
+
+
+  if (window.matchMedia("(max-width: 1200px)").matches) {
+
+    let cards = document.querySelectorAll('.section4__item_inner');
+
+    cards.forEach((card) => card.addEventListener('touchend', flip));
+    function flip(){
+      
+      let currentStyle = this.getAttribute("style");
+      if (currentStyle=='transform: rotateY(180deg);') {
+        this.style.transform = "rotateY(0deg)";
+      } else {
+        this.style.transform = "rotateY(180deg)";
+      }
+
+    }
+  }
 
 
